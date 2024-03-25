667f3ecb337cf68db27fd3b7d37a29ed338dd5cb
Brandon Noone <b.noone@outlook.com>
2024-03-25 14:24:07 +0000

    Added message for environment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = {
+    title: "Azure App Service",
+    message: process.env.message || "Dev Environment"
+  }
+  res.render('index', data);
 });
 
 module.exports = router;
